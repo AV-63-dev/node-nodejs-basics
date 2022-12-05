b82a8965e532a62c2f5e7fe8710e781a53a47628
AV-63-dev <amanin63@mail.ru>
2022-12-06 01:55:36 +0400

    did task src/fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { access, constants, writeFile } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url'
+
 const create = async () => {
-    // Write your code here 
+    const file = join(fileURLToPath(import.meta.url), '../files', 'fresh.txt');
+
+    access(file, constants.F_OK, err_task => {
+        if (err_task) {
+            writeFile(file, 'I am fresh and young', err => { 
+                if (err) throw err;
+                console.log('Файл создан 😁');
+            });
+        } else {
+            throw new Error('FS operation failed');
+        };
+    });
 };
 
 await create();
